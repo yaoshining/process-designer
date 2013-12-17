108c1380a570013136e337f6f701e7da96e5cb29
yao <ysnlvlqw@msn.com>
2013-12-17 16:47:04 +0800

    优化前端框架，使用模块化开发

--- static/js/app/main.js
+++ static/js/app/main.js
@@ -3,39 +3,6 @@
  */
 define(["raphael","jquery-ui","raphael-extend"], function() {
     $(document).ready(function(){
-//        var center = document.getElementById("center");
-//        $(center).on("mousemove",function(e){
-//            console.log(e.clientX+","+ e.clientY);
-//        });
-//        var paper = new Raphael(center,$(center).innerWidth(),$(center).innerHeight());
-//        var circle = paper.circle(50,50,20);
-//        circle.node.id = "circle";
-//        var rect = paper.rect(150,30,100,80,10);
-//        rect.node.id = "rect";
-//        rect.attr({
-//            "fill": "#ffffcc",
-//            "stroke": "#000",
-//            title: "Rect"
-//        });
-//        circle.attr({
-//            "fill": "#fff",
-//            "stroke": "#000"
-//        });
-//        var dragger = function () {
-//            this.ox = this.type == "rect" ? this.attr("x") : this.attr("cx");
-//            this.oy = this.type == "rect" ? this.attr("y") : this.attr("cy");
-//            this.animate({"fill-opacity": .2}, 500);
-//        };
-//        var up = function () {
-//            this.animate({"fill-opacity": 1}, 500);
-//        };
-//        var move = function(dx,dy){
-//            var attr = this.type == "rect" ? {x: this.ox + dx, y: this.oy + dy} : {cx: this.ox + dx, cy: this.oy + dy};
-//            this.attr(attr);
-//            paper.connection(conn);
-//        };
-//        circle.drag(move,dragger,up);
-//        rect.drag(move,dragger,up);
-//        var conn = paper.connection(circle, rect, "#000");
+
     });
 });
